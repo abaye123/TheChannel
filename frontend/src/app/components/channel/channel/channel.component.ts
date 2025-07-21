eae625e51035fbf00fd8c12cd6b8450fa8773469
IsraeliGuitar <rabtests@gmail.com>
2025-07-21 07:04:07 +0000

    feat: enhance chat and advertising components with responsive behavior

--- frontend/src/app/components/channel/channel/channel.component.ts
+++ frontend/src/app/components/channel/channel/channel.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from "../chat/chat.component";
 import { AdvertisingComponent } from "../advertising/advertising.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel',
   imports: [
     ChatComponent,
-    AdvertisingComponent
+    AdvertisingComponent,
+    CommonModule
   ],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
+
+  constructor() { }
+  advertisingEnabled: boolean = true;
 
 }
