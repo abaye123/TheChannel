07a0f5e12ee6b69ce70c22ad81545d936260179e
abaye <8140hp@gmail.com>
2025-11-21 03:07:19 +0200

    refactor(markdown): replace custom YouTube Shorts icon with official SVG

--- frontend/src/app/markdown.config.ts
+++ frontend/src/app/markdown.config.ts
@@ -51,15 +51,18 @@ const customEmbedExtension = {
           const imgStyle = isShorts
             ? 'width: 100%; aspect-ratio: 9/16; object-fit: cover;'
             : 'width: 100%;';
-          
+
           // Different icons for Shorts vs regular videos
           if (isShorts) {
-            // YouTube Shorts icon (custom SVG)
-            return `<div style="${containerStyle}">
-              <img youtubeid="${id}" isshorts="true" src="https://i.ytimg.com/vi/${id}/hqdefault.jpg" class="img-fluid" style="${imgStyle}">
-              <svg youtubeid="${id}" isshorts="true" style="position: absolute; place-self: anchor-center; width: 50px; height: 50px; cursor: pointer;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="5" y="3" width="14" height="18" rx="3" fill="#FF0000"/>
-                <path d="M10 8.5L15 12L10 15.5V8.5Z" fill="white"/>
+            // YouTube Shorts icon (Official SVG)
+            return `<div style="${containerStyle}; position: relative; display: inline-block;">
+              <img youtubeid="${id}" isshorts="true" src="https://i.ytimg.com/vi/${id}/hqdefault.jpg" class="img-fluid" style="${imgStyle}; display: block;">
+              <svg youtubeid="${id}" isshorts="true" 
+                style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 50px; height: 50px; cursor: pointer; filter: drop-shadow(0 2px 4px rgba(0,0,0,0.3));" 
+                viewBox="0 0 98.94 122.88" 
+                  xmlns="http://www.w3.org/2000/svg">
+                <path d="M63.49 2.71c11.59-6.04 25.94-1.64 32.04 9.83 6.1 11.47 1.65 25.66-9.94 31.7l-9.53 5.01c8.21.3 16.04 4.81 20.14 12.52 6.1 11.47 1.66 25.66-9.94 31.7l-50.82 26.7c-11.59 6.04-25.94 1.64-32.04-9.83-6.1-11.47-1.65-25.66 9.94-31.7l9.53-5.01c-8.21-.3-16.04-4.81-20.14-12.52-6.1-11.47-1.65-25.66 9.94-31.7l50.82-26.7zM36.06 42.53l30.76 18.99-30.76 18.9V42.53z" fill="#f40407"/>
+                <path d="M36.06,42.53 V 80.42 L 66.82,61.52Z" fill="#fff"/>
               </svg>
             </div>`;
           } else {
