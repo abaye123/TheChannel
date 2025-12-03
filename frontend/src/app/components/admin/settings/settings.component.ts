9d8f39c9c8ae996f290fce3e21296877db6f2156
abaye <8140hp@gmail.com>
2025-12-03 06:15:30 +0200

    feat(admin): add loading states and improve error handling in settings

--- frontend/src/app/components/admin/settings/settings.component.ts
+++ frontend/src/app/components/admin/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../../services/admin.service';
-import { NbButtonModule, NbCardModule, NbToastrService, NbIconModule, NbInputModule } from "@nebular/theme";
+import { NbButtonModule, NbCardModule, NbToastrService, NbIconModule, NbInputModule, NbSpinnerModule } from "@nebular/theme";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Setting } from '../../../models/setting.model';
@@ -12,6 +12,7 @@ import { Setting } from '../../../models/setting.model';
     NbButtonModule,
     NbIconModule,
     NbInputModule,
+    NbSpinnerModule,
     CommonModule,
     FormsModule
   ],
@@ -21,6 +22,7 @@ import { Setting } from '../../../models/setting.model';
 export class SettingsComponent implements OnInit {
 
   settings: Setting[] = [];
+  isLoading: boolean = true;
   setInProgress: boolean = false;
 
   constructor(
@@ -29,16 +31,29 @@ export class SettingsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.adminService.getSettings()
-      .then(settings => this.settings = settings || []);
+      .then(settings => {
+        this.settings = settings || [];
+        this.isLoading = false;
+      })
+      .catch(() => {
+        this.isLoading = false;
+        this.tostService.danger('', 'שגיאה בטעינת ההגדרות');
+      });
   }
 
   saveSettings() {
     this.setInProgress = true;
     this.adminService.setSettings(this.settings)
-      .then(() => this.tostService.success('', 'השינוים נשמרו בהצלחה!'))
-      .catch(() => this.tostService.danger('', 'שגיאה בשמירת השינוים'));
-    this.setInProgress = false;
+      .then(() => {
+        this.tostService.success('', 'השינוים נשמרו בהצלחה!');
+        this.setInProgress = false;
+      })
+      .catch(() => {
+        this.tostService.danger('', 'שגיאה בשמירת השינוים');
+        this.setInProgress = false;
+      });
   }
 
   removeSetting(index: number) {
